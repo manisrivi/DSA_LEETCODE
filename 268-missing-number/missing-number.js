a76d0c75df3f3e63d2cef71364c6777fbc33fda1
Manikandan S <95971849+manisrivi@users.noreply.github.com>
2026-03-15 21:43:02 +0530

    Time: 0 ms (100.00%) | Memory: 55.6 MB (88.96%) - LeetSync

---
+++ 268-missing-number/missing-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    
+    const n = nums.length;
+    let totalSum = n * (n+1) / 2;
+    let partialSum = 0;
+
+    for (let i=0; i<n; i++) {
+        partialSum = partialSum + nums[i];
+    }
+    return totalSum - partialSum
+
+};
