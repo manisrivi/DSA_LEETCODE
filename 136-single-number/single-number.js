5b79c2788078cf587c29f9055dff6aab3fa7bf67
Manikandan S <95971849+manisrivi@users.noreply.github.com>
2026-03-31 09:31:19 +0530

    Time: 9 ms (36.39%) | Memory: 62.9 MB (5.09%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -3,9 +3,19 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let xor = 0;
+      let map = new Map();
+
     for (let i=0; i<nums.length; i++) {
-        xor = xor ^ nums[i];
+        if (map.has(nums[i])) {
+            map.set(nums[i], map.get(nums[i]) + 1);
+        } else {
+            map.set(nums[i], 1);
+        }
     }
-    return xor
+
+    for (let [key, value] of map) {
+        if (value === 1) {
+            return key;
+        }
+    }
 };
